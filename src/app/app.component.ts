8429dac54bba39d63c781ea2d86ddf7950a39be5
Leilani Leach <leilanileach@yahoo.com>
2019-02-05 09:44:38 -0800

    Add initial files for an Angular Firebase chat web app

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+}
