b4b8f028817b8d770f4842454c8a03bc7e180e87
Leilani Leach <leilanileach@yahoo.com>
2019-02-06 10:10:04 -0800

    create footer for message input

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -2,20 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { MessageService } from 'app/message.service';
 import { Router } from '@angular/router';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/scan';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
+
 export class MessageComponent implements OnInit {
-messages: FirebaseListObservable<any[]>;
+  messages: FirebaseListObservable<any[]>;
   constructor(private service: MessageService, private router: Router) {
-   }
+    
+  }
 
   ngOnInit() {
     this.messages = this.service.getMessages();
   }
 
+  
 
 }
